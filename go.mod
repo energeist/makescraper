6bb34b95e20db39f3025f0c7fbb433e3a9e6713e
Mark Rattle <mark.rattle@gmail.com>
2024-04-12 14:15:55 -0400

    successfully scraping multiple tickers

--- go.mod
+++ go.mod
@@ -3,12 +3,12 @@ module github.com/energeist/makescraper
 go 1.21
 
 require (
+	github.com/chromedp/cdproto v0.0.0-20240328024531-fe04f09ede24
 	github.com/chromedp/chromedp v0.9.5
 	github.com/jbrodriguez/mlog v0.0.0-20180805173533-cbd5ae8e9c53
 )
 
 require (
-	github.com/chromedp/cdproto v0.0.0-20240202021202-6d0b6a386732 // indirect
 	github.com/chromedp/sysutil v1.0.0 // indirect
 	github.com/gobwas/httphead v0.1.0 // indirect
 	github.com/gobwas/pool v0.2.1 // indirect
--- scrape.go
+++ scrape.go
@@ -4,7 +4,10 @@ import (
 	// "encoding/csv"
 	"context"
 	"fmt"
+	"strings"
+	"time"
 
+	"github.com/chromedp/cdproto/cdp" // need this for the Type to capture multiple nodes
 	"github.com/chromedp/chromedp"
 	"github.com/jbrodriguez/mlog"
 )
@@ -15,25 +18,52 @@ func main() {
 	ctx, cancel := chromedp.NewContext(context.Background())
 	defer cancel()
 
-	var ticker, name, percentChange string
+	ctx, cancel = context.WithTimeout(ctx, 60*time.Second)
+	defer cancel()
+
+	var results []*cdp.Node
+
 	err := chromedp.Run(ctx,
 		chromedp.Navigate(`https://finance.yahoo.com`),
-		// Wait for the element to be visible
-		chromedp.WaitVisible(`section[data-yaft-module="tdv2-applet-crypto_currencies"]`),
-		// Extract the text of the element
-		chromedp.Text(`section[data-yaft-module="tdv2-applet-crypto_currencies"] > table > tbody > tr > td:first-child > a`, &ticker),
-		chromedp.Text(`section[data-yaft-module="tdv2-applet-crypto_currencies"] > table > tbody > tr > td:first-child > p`, &name),
-		chromedp.Text(`section[data-yaft-module="tdv2-applet-crypto_currencies"] > table > tbody > tr > td:last-child > fin-streamer > span`, &percentChange),
-	)
-	if err != nil {
-		mlog.Warning("Failed to scrape: %v", err)
-		mlog.Error(err)
-	}
+		// Use chromedp.Nodes to retrieve all nodes that match the selector
+		chromedp.WaitVisible(`section[data-yaft-module="tdv2-applet-crypto_currencies"] > table > tbody > tr > td:last-child > fin-streamer > span`),
+		chromedp.Nodes(`section[data-yaft-module="tdv2-applet-crypto_currencies"] > table > tbody > tr > td:first-child > a`, &results, chromedp.ByQueryAll),
+)
+if err != nil {
+	mlog.Warning("Failed to scrape: %v", err)
+	mlog.Error(err)
+}
 
-	dataPoint := createDataPoint(ticker, name, percentChange)
+// Print the results
+for i, result := range results {
+	ticker := result.AttributeValue("href")
+	ticker = strings.TrimPrefix(ticker, "/quote/")
+	fmt.Printf("Element %d: %v\n", i+1, ticker)
+}
+	// This was successfully grabbing one result from the many available
 
-	fmt.Printf("Scraped text: %s, %s, %s\n", dataPoint.Ticker, dataPoint.Name, dataPoint.PercentChange)
-	mlog.Info("Scraped text: %s, %s, %s\n", dataPoint.Ticker, dataPoint.Name, dataPoint.PercentChange)
+	// var tickers, names, percentChanges string
+	// err := chromedp.Run(ctx,
+	// 	chromedp.Navigate(`https://finance.yahoo.com`),
+	// 	// Wait for the element to be visible
+	// 	chromedp.WaitVisible(`section[data-yaft-module="tdv2-applet-crypto_currencies"] > table > tbody > tr > td:last-child > fin-streamer > span`),
+	// 	// Extract the text of the element
+	// 	chromedp.Text(`section[data-yaft-module="tdv2-applet-crypto_currencies"] > table > tbody > tr > td:first-child > a`, &tickers, chromedp.ByQueryAll),
+	// 	chromedp.Text(`section[data-yaft-module="tdv2-applet-crypto_currencies"] > table > tbody > tr > td:first-child > p`, &names, chromedp.ByQueryAll),
+	// 	chromedp.Text(`section[data-yaft-module="tdv2-applet-crypto_currencies"] > table > tbody > tr > td:last-child > fin-streamer > span`, &percentChanges, chromedp.ByQueryAll),
+	// )
+	// if err != nil {
+	// 	mlog.Warning("Failed to scrape: %v", err)
+	// 	mlog.Error(err)
+	// }
+
+	// fmt.Printf("scraped tickers: %v\n", tickers)
+	// for _, item := range tickers {
+	// 	dataPoint := createDataPoint(string(tickers[item]), string(names[item]), string(percentChanges[item]))
+
+	// 	fmt.Printf("Scraped text: %s, %s, %s\n", dataPoint.Ticker, dataPoint.Name, dataPoint.PercentChange)
+	// 	mlog.Info("Scraped text: %s, %s, %s\n", dataPoint.Ticker, dataPoint.Name, dataPoint.PercentChange)
+	// }
 }
 
 type ScrapedItem struct {
